0e792d1de16c923fa2a1b72fc44f424af9d2eb23
GiORDiA <giordiariel.fullstackdev@gmail.com>
2022-09-02 10:05:09 -0300

    Creación del BackEnd SpringBoot

---
+++ src/main/java/com/portfolioweb/BackEndSpringBootAEG75/BackEndSpringBootAeg75Application.java
@@ -0,0 +1,13 @@
+package com.portfolioweb.BackEndSpringBootAEG75;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSpringBootAeg75Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSpringBootAeg75Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolioweb/BackEndSpringBootAEG75/BackEndSpringBootAeg75ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioweb.BackEndSpringBootAEG75;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSpringBootAeg75ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
