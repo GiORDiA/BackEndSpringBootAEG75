14d462b2a189fc773f98305cf49e637b68976281
GiORDiA <giordiariel.fullstackdev@gmail.com>
2022-09-02 23:05:59 -0300

    Creación de los Package varios con sus respectivos metodos e import

---
+++ src/main/java/com/portfolioweb/BackEndSpringBootAEG75/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolioweb.BackEndSpringBootAEG75.Repository;
+
+import com.portfolioweb.BackEndSpringBootAEG75.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
